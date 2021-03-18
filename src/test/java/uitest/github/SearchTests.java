c02686b4bcf5c699cc502eaac719ff7565f37ea7
John Smoker <testgits@yandex.ru>
2021-03-18 18:33:27 +0500

    Added search tests

--- src/test/java/uitest/github/SearchTests.java
+++ src/test/java/uitest/github/SearchTests.java
@@ -1,6 +1,31 @@
 package uitest.github;
 
+import org.junit.jupiter.api.Test;
 import uitest.AbstractBase;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SearchTests extends AbstractBase {
+
+    @Test
+    void searchInRepoTest(){
+        open("/selenide/selenide");
+
+        $(byName("q")).setValue("dragAndDropTo");
+        $$("#jump-to-results li").findBy(text("In this repository")).click();
+        $$(".width-full").findBy(text("public class DragAndDropTo")).parent().parent().click();
+        $("[itemprop = 'text']").shouldHave(text("public class DragAndDropTo"));
+    }
+
+    @Test
+    void searchInOrgTest(){
+        open("/selenide/selenide");
+
+        $(byName("q")).setValue("appium");
+        $$("#jump-to-results li").findBy(text("In this organization")).click();
+        $(".repo-list a[href = '/selenide/selenide-appium']").click();
+        $("#readme").shouldHave(text("Selenide adaptor for Appium framework"));
+    }
 }
